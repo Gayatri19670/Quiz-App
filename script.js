59ef6ab04f9d7a9a7b6b3449933d769c08fb81f7
Gayatri19670 <samirgupta19670@gmail.com>
2020-12-17 18:11:10 +0530

    Update Script

--- script.js
+++ script.js
@@ -85,23 +85,36 @@ const quizData = [{
 
 ];
 
-const questionE1 = document.getElementById('question');
-const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
-const d_text = document.getElementById('d_text');
-const submitBtn = document.getElementById('submit');
+const questionEl = document.getElementById("question");
 
-let currentQuestion = 0;
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+console.log(questionEl, quizData);
+
+let currentQuiz = 0;
 
 loadQuiz();
 
 function loadQuiz() {
     const currentQuizData = quizData[currentQuiz];
-    questionE1.innerText = currentQuizData.question;
+    questionEl.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
-    currentQuestion++;
+
+    console.log(currentQuizData);
 }
+
+
+submitBtn.addEventListener("click", () => {
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+        loadQuiz();
+    } else {
+        alert("You finished! Get yourself an Orange Lemonade");
+    }
+});
